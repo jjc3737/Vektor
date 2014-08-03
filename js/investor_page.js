410357c2e5560be72c36cb0ae9787de1bcf5de03
jjc3737 <jaeheejanechung@gmail.com>
2014-08-02 19:21:26 -0700

    about to pull my hair out
    now the get free video is slowing down a lot on my computer at least.
    perhaps we just keep that one and see what the response is then move
    on? focus on the copy more. Wanted to say hardware startup somewhere

--- js/investor_page.js
+++ js/investor_page.js
@@ -4,5 +4,43 @@ $( document ).ready(function() {
 		$("#landingVideo").attr( "controls", true );
 	}
 	$('body').scrollspy({ target: '.right_header_position'})
+	//var myVideo = _V_("landingVideo");
+	//function onComplete() { 
+	//	var landingVideo = document.getElementsByTagName('video')[0];
+	//	var videoPlaying = landingVideo.currentSRC; 
+	//	var ext = videoPlaying.substr(videoPlaying.lastIndexof(".")); 
+	//	landingVideo.src = 'video/eu'+ext
+	//	landingVideo.load();
+	//	landingVideo.play();
+	//	landingVideo.removeEvent("ended", onComplete);
+	
+	//};
+	//myVideo.addEvent("ended", onComplete);
+	
+	var videoSource = new Array(); 
+	videoSource[0] = document.getElementsById("landingVideo").currentSRC;
+	videoSource[1] = document.getElemtnsById("landingVideo2").currentSRC; 
+	videoSource[2] = document.getElemtnsById("landingVideo3").currentSRC;
+	var videoCount = videoSource.length; 
+	
+	function videoPlay(videoNum) { 
+		document.getElementById("landingVideo").setAttribute("src", videoSource[videoNum]);
+		document.getElementById("landingVdieo").load(); 
+		document.getElementById("landingVideo").play();
+	}
+	document.getElementById('landingVideo').addEventListener('ended',myHandler,false);
+	function myHandler() {
+	i++;
+	if(i == (videoCount-1)){
+	i = 0;
+	videoPlay(i);
+	}
+	else{
+	videoPlay(i);
+	}
+        
+	       }
+	
+	
 });
 
