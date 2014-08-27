2aec71daf84a0dcd2ec08edd369e9bd7eb300f57
jjc3737 <jaeheejanechung@gmail.com>
2014-08-26 18:14:59 -0700

    trial mobile

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 $( document ).ready(function() {
     var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false );
-	if(iOS){
-		$("#landingVideo").attr( "controls", true );
+	if(window.innerWidth <= 800 && window.innerHeight <= 600){
+		$('#home').empty();
+		$('#home').prepend('<img id="mobileLanding" src="getfree.jpeg" />')
+		
 	}
 	$('body').scrollspy({ target: '.right_header_position'})
+	
+	
 });
