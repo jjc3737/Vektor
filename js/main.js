8e14e014d189eb717c9e07551d3fc010b63a964b
jjc3737 <jaeheejanechung@gmail.com>
2014-08-26 19:18:37 -0700

    backrgound

--- js/main.js
+++ js/main.js
@@ -2,10 +2,7 @@ $( document ).ready(function() {
     var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false );
 	if(window.innerWidth <= 800 && window.innerHeight <= 600){
 		$('#home').empty();
-		$('#home').prepend('<img id="mobileLanding" src="getfree.jpeg" />')
-		$('#home').prepend('<img id="mobileLanding" src="yerba.jpeg" />')
-		$('#home').prepend('<img id="mobileLanding2" src="dolores.jpeg" />')
-		$('#mainTitle').css('color', '#525151');
+		$('#home').css({'background-color': 'black'});
 		
 	}
 	$('body').scrollspy({ target: '.right_header_position'});
