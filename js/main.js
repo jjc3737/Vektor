142a8f863f8b8891d4072449b689fe9ec663dee0
jjc3737 <jaeheejanechung@gmail.com>
2014-08-26 19:06:02 -0700

    test

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ $( document ).ready(function() {
     var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false );
 	if(window.innerWidth <= 800 && window.innerHeight <= 600){
 		$('#home').empty();
-		$('#home').prepend('<img id="mobileLanding" src="getfree.jpeg" />
-		<img id="mobileLanding" src="yerba.jpeg" />
-		<img id="mobileLanding2" src="dolores.jpeg" />')
+		$('#home').prepend('<img id="mobileLanding" src="getfree.jpeg" />')
+		$('#home').prepend('<img id="mobileLanding" src="yerba.jpeg" />')
+		$('#home').prepend('<img id="mobileLanding2" src="dolores.jpeg" />')
 		
 	}
 	$('body').scrollspy({ target: '.right_header_position'})
